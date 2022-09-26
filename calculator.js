3c642f28069d1aece02d5f21f4af0cf2231d6c82
Amina Muumin <aminamu7@gmail.com>
2022-09-25 23:07:07 -0500

    calculator working but array is not emptying after each input

--- calculator.js
+++ calculator.js
@@ -1,47 +1,123 @@
 // an array to store all of our cars
-let employee = [];
-
-// // max garage size
-// let garageSize = 2;
-
-
 $(document).ready(readyNow);
+
+
+
+let employees = [
+    //noemployees yet
+];
 
 function readyNow() {
   console.log("DOM is loaded!");
 
-  // add click event handler that calls the addNewCar function
-  $('#employee-form').on('submit', addNewEmployee);
+  // add click event handler that calls the addNewEmployee function
+  $('#employee-form').on('submit', onSubmitEmployee);
 
-  $('#employee').on('click', '.remove-btn', removeEmployee);
+//   $('#employee').on('click', '.remove-btn', removeEmployee);
+  $('#employeesTable').on('click', '.deleteBtn', onDelete);
+
+
+
 }
+function onDelete() {
+    console.log('in onDelete. Which button did we click? $(this) one!', $('#deleteBtn'));
 
-function addNewEmployee(evt) {
-  evt.preventDefault();
+    // BASE: remove the <tr> directly from the DOM
+    // <button>    <td>      <tr>
+    $(this).parent().parent().remove();
 
-  // console.log('bool?', $('#year-input'));
-
-  // if one or more inputs is blank, don't add a new employee obj to array
-  if (
-    $('#firstName-input').val() === '' 
-    || $('#lastName-input').val() === ''
-    || $('#id-input').val() === ''
-    || $('#title-input').val() === ''
-    || $('#annualSalary-input').val() === ''
-  ) {
-    alert('Values plz!');
-    return;
-  }
-  // console.log('working!');
-
-  // get input values
-  let newEmployee = {
+}
+function onSubmitEmployee(evt) {
+    // Don't reload the page, plz
+        evt.preventDefault()
+       
+    console.log('onSubmitEmployee', $('#submit'));
+    
+    // // // Update State....
+    let newEmployee = {
     firstName: $('#firstName-input').val(),
     lastName: $('#lastName-input').val(),
     id: $('#id-input').val(),
     title: $('#title-input').val(),
     annualSalary: $('#annualSalary-input').val(),
-  };
+    };
+    console.log('newEmployee is', newEmployee);
+    employees.push(newEmployee)  // TODO: add object to state
+    console.log('employees are now', employees);
+
+    totalMonthly();
+
+    // Render
+    render(); 
+    // totalsalary = annualSalary[i]++;
+    // totalmonthly = (totalsalary)/12
+
+}
+function totalMonthly(){
+    let totalSalary = 0;
+    $('.totalMonthlySalary').empty();
+    for (let salary of employees){
+      totalSalary+= Number(salary.annualSalary);
+    }
+    $('.totalMonthlySalary').append(`Total Monthly Salary: ${totalSalary / 12}`);
+    if((totalSalary/12) >= 0){
+        $('.totalMonthlySalary').css('font-color', 'black');
+      }
+   
+  }
+
+function render() {
+    // Empty the table
+    // $('#employeesTable').empty();
+
+    // Then add everything back into it!
+    for (let staff of employees) {
+        // Adding a <tr> for each post
+        $('#employeesTable').append(`
+            <tr>
+                <td>${staff.firstName}</td>
+                <td>${staff.lastName}</td>
+                <td>${staff.id}</td>
+                <td>${staff.title}</td>
+                <td>${staff.annualSalary}</td>
+                
+                <td>
+                    <button class="deleteBtn">
+        
+                    </button>
+                </td>
+            </tr>
+        `);
+    }
+
+}
+
+// function addNewEmployee(evt) {
+//   evt.preventDefault();
+
+//   // console.log('bool?', $('#year-input'));
+
+//   // if one or more inputs is blank, don't add a new employee obj to array
+//   if (
+//     $('#firstName-input').val() === '' 
+//     || $('#lastName-input').val() === ''
+//     || $('#id-input').val() === ''
+//     || $('#title-input').val() === ''
+//     || $('#annualSalary-input').val() === ''
+//   ) {
+//     alert('Values plz!');
+//     return;
+//   }
+//   // console.log('working!');
+
+  // get input values
+//   let newEmployee = {
+//     firstName: $('#firstName-input').val(),
+//     lastName: $('#lastName-input').val(),
+//     id: $('#id-input').val(),
+//     title: $('#title-input').val(),
+//     annualSalary: $('#annualSalary-input').val(),
+//   };
 
   // add to array
 //   employeelist.push(newEmployee);
@@ -49,15 +125,15 @@ function addNewEmployee(evt) {
 //   checkNumEmployees();
 
   // clear the inputs
-  $('#firstName-input').val('');
-  $('#lastName-input').val('');
-  $('#id-input').val('');
-  $('#title-input').val('');
-  $('#annualSalary-input').val('');
-  // anything else?
-  // re-render fresh values!
-  render();
-}
+//   $('#firstName-input').val('');
+//   $('#lastName-input').val('');
+//   $('#id-input').val('');
+//   $('#title-input').val('');
+//   $('#annualSalary-input').val('');
+//   // anything else?
+//   // re-render fresh values!
+//   render();
+// }
 
 // function removePerson() {
 //   // console.log('bye!');
@@ -78,97 +154,41 @@ function addNewEmployee(evt) {
 //   }
 // }
 
-function render() {
-  // update the DOM
-  $('#employee').empty();
+// function render() {
+//   // update the DOM
+//   $('#employee').empty();
 
-  // append all car objs in garage array
-  for (let person of employee) {
-    $('#employee').append(`
-      <li>
-        ${person.firstName} ${person.lastName} ${person.id} ${person.title} ${person.annualSalary}
-        <button class="remove-btn">Remove</button>
-      </li>
-    `);
-  }
-  let Employees = [
-    /* no posts yet  */
-];
-// let darkMode = false;
+//   
+//   for (let person of employee) {
+//     $('#employee').append(`
+//       <li>
+//         ${person.firstName} ${person.lastName} ${person.id} ${person.title} ${person.annualSalary}
+//         <button class="remove-btn">Remove</button>
+//       </li>
+//     `);
+//   }
+//   let Employees = [
+//     /* no posts yet  */
+// ];
 
-function onReady() {
-    // Event
-    $('#messageForm').on('submit', onSubmitEmployee);
+// function onReady() {
+//     // Event
+//     $('#messageForm').on('submit', onSubmitEmployee);
 
-    // "The delete button ain't there yet" - Edan
-    // console.log('in onReady', $('.deleteBtn'));
-    // $('.deleteBtn').on('click', onDelete);
 
-    // Listen for clicks anywhere on the table
-    // but only call onDelete if it's a `.deleteBtn` element
-    //
-    // aka "Event Delegation"
-    $('#employeesTable').on('click', '.deleteBtn', onDelete);
-}   
+//     // console.log('in onReady', $('.deleteBtn'));
+//     // $('.deleteBtn').on('click', onDelete);
 
-function onDelete() {
-    console.log('in onDelete. Which button did we click? $(this) one!', $(this));
+//     // Listen for clicks anywhere on the table
+//     // but only call onDelete if it's a `.deleteBtn` element
+//     //
+//     // aka "Event Delegation"
+//     $('#employeesTable').on('click', '.deleteBtn', onDelete);
+// }   
 
-    // BASE: remove the <tr> directly from the DOM
-    // <button>    <td>      <tr>
-    $(this).parent().parent().remove();
 
-    // STRETCH: somehow update the posts array
-    // to remove the item we clicked on
-    // then call
-    // render() to render that state;
-}
 
 // Event handler
-function onSubmitEmployee(evt) {
-    // Don't reload the page, plz
-    evt.preventDefault();
 
-    console.log('onSubmitEmployee', $(this));
-    
-    // Update State....
-    let newEmployee = {
-        firstName: $('#firstName-input').val(),
-        lastName: $('#lastName-input').val(),
-        ID: $('id-input').val(),
-        title: $('#title-Input').val(),
-        annualSalary: $('#annualSalary-Input').val()
-    };
-    console.log('newEmployee is', newEmployee);
-    employee.push(newEmployee)  // TODO: add object to state
-    console.log('employees are now', employee);
 
-    // Render
-    render(); 
-}
 
-function render() {
-    // Empty the table
-    $('#employeesTable').empty();
-
-    // Then add everything back into it!
-    for (let post of posts) {
-        // Adding a <tr> for each post
-        $('#employeesTable').append(`
-            <tr>
-                <td>${employee.firstName}</td>
-                <td>${employee.lastName}</td>
-                <td>${employee.id}</td>
-                <td>${employee.title}</td>
-                <td>${employee.annualSalary}</td>
-                <td>
-                    <button class="deleteBtn">
-                        ☠️
-                    </button>
-                </td>
-            </tr>
-        `);
-    }
-
-}
-}
